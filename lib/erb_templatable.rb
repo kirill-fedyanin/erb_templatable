e8eaf86b031f7ea080ef881f9ccdf14dc2b2ae26
Kirill <kirill.fedyanin@gmail.com>
2016-10-30 18:52:41 +0300

    First realization

--- lib/erb_templatable.rb
+++ lib/erb_templatable.rb
@@ -1,5 +1,9 @@
-class ErbTemplatable
-  def self.hello
-    puts "Hi, bro!"
+module ErbTemplatable
+  def erb_template(file_name, dir_name=nil, subdir="template/", bind=binding)
+    if dir_name.nil?
+      dir_name = File.dirname caller[0].partition(":").first
+    end
+    template_file = File.read("#{dir_name}/#{subdir}#{file_name}")
+    ERB.new(template_file).result bind
   end
 end
